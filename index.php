daf5e0b274188f704abcd416c2e2dbb958842da1
alessandro <alebeni02@gmail.com>
2021-05-03 00:07:11 +0200

    ,

---
+++ index.php
@@ -0,0 +1,155 @@
+<?php
+  $faqs = [
+    '¿Cómo está implementando Google la reciente sentencia dictada por el Tribunal de Justicia de la Unión Europea (TJUE) sobre el derecho al olvido?' => [
+      [
+          'El reciente <span>fallo del Tribunal de Justicia de la Unión Europea</span> tiene profundas consecuencias para los motores de búsqueda en Europa. El tribunal ha considerado que determinados usuarios tienen derecho a solicitar a los motores de búsqueda como Google que eliminen los resultados de consultas que incluyan su nombre. Para ello, los resultados mostrados deben considerarse inadecuados, irrelevantes o ya no relevantes, o excesivos.',
+
+          'Desde que esta sentencia se publicó el 13 de mayo de 2014, hemos estado trabajando contra reloj para cumplirla. Se trata de un proceso complicado porque necesitamos evaluar cada solicitud de forma individual y ponderar los derechos de la persona a controlar sus datos personales con el derecho del público a conocer y distribuir información.',
+
+          'Si quieres enviar una solicitud de eliminación, rellena este <span>formulario web</span>. Recibirás una respuesta automática en la que se confirmará que hemos recibido tu solicitud. A continuación, evaluaremos tu caso (ten en cuenta que esta evaluación puede tardar algún tiempo, puesto que ya hemos recibido muchas de estas solicitudes). Al evaluar tu solicitud, tendremos en cuenta si los resultados incluyen información obsoleta sobre tu vida privada, así como si existe un interés público en lo que respecta a la información que permanece en los resultados de búsqueda de Google (por ejemplo, si está relacionada con estafas financieras, con negligencia profesional, con condenas penales o con tu conducta como funcionario público, tanto electo como designado). Se trata de decisiones difíciles y, como organización privada, es posible que no nos encontremos en una posición adecuada para decidir sobre tu caso. Si no estás de acuerdo con nuestra decisión, puedes ponerte en contacto con la autoridad de protección de datos local.',
+
+          'Esperamos colaborar estrechamente con las autoridades de protección de datos y con otros organismos en los próximos meses a medida que perfeccionamos nuestro enfoque. La sentencia del TJUE constituye un cambio considerable para los motores de búsqueda. Aunque nos preocupa su impacto, también creemos que es importante respetar la decisión del tribunal, por lo que estamos trabajando para diseñar un proceso que respete la ley.',
+
+          'Al buscar un nombre, es posible que aparezca un aviso indicando que los resultados se pueden haber modificado de acuerdo con la ley de protección de datos europea. Google muestra este aviso en Europa cuando un usuario busca la mayoría de los nombres, no solo las páginas que se han visto afectados por una eliminación.'
+      ]
+    ],
+    '¿Cómo protege Google mi privacidad y mi información?' => [
+      [
+          'Somos conscientes de que la seguridad y la privacidad son importantes para ti y, para nosotros, también lo son. Una de nuestras prioridades es ofrecer la mayor seguridad posible, así como garantizar que tu información esté a salvo y que puedas acceder a ella en cualquier momento.',
+
+          'Trabajamos constantemente para garantizar una seguridad sólida, proteger tu privacidad y hacer que Google sea lo más eficaz y eficiente para ti. Invertimos cientos de millones de dólares cada año en seguridad y disponemos de los expertos en seguridad de datos más importantes del mundo para mantener tu información protegida. También hemos creado herramientas de privacidad y seguridad fáciles de utilizar como, por ejemplo, el Panel de Control de Google, la verificación en dos pasos y la Configuración de Anuncios. Por tanto, en lo que respecta a la información que compartes con Google, tú tienes el control.',
+
+          'Para obtener más información sobre la seguridad online, como lo que puedes hacer para protegeros a ti y a tu familia en Internet, consulta la página del <span>Centro de seguridad de Google</span>.',
+
+          '<span>Obtén más información</span> sobre cómo mantenemos la privacidad y la seguridad de tu información personal y sobre cómo te garantizamos el control de esa información.'
+      ]
+  ],
+
+ '¿Por qué está asociada mi cuenta a un país?' => [
+      'info' =>[
+        'Tu cuenta se asocia a un país (o territorio) en los Términos del Servicio para poder determinar dos cosas: <br>
+        <ol>
+           <li>La entidad asociada de Google que proporciona los servicios, trata tu información y es responsable de cumplir con las leyes de privacidad aplicables. Por lo general, Google ofrece sus servicios para consumidores a través de estas dos empresas:
+              <ol>
+                 <li>Google Ireland Limited, si el usuario se encuentra en el Espacio Económico Europeo (países de la UE, Islandia, Liechtenstein y Noruega) o en Suiza</li>
+                 <li>Google LLC, con sede en Estados Unidos, si el usuario se encuentra en cualquier otra parte del mundo</li>
+              </ol>
+            </li>
+             <li>La versión específica de los términos que rigen nuestra relación, que depende de la legislación local</li>
+        </ol>
+        <p>Ten en cuenta que los servicios de Google son básicamente los mismos, con independencia de la entidad asociada que los proporcione o del país al que esté asociada tu cuenta.</p>',
+
+
+      ],
+      'info2' =>['<h4>Determinar el país asociado a tu cuenta</h4>
+                  <p>Cuando creas una cuenta de Google, la asociamos a un país en función de dónde la creaste. Las cuentas que hayas creado hace más de un año se asocian al país desde el que sueles acceder a los servicios de Google. Suele ser el país en el que has estado la mayor parte del tiempo durante el último año.</p>
+                  <p>Por lo general, viajar frecuentemente no afecta al país asociado a la cuenta. Si te mudas a otro país, podemos tardar aproximadamente un año en actualizar el país asociado a tu cuenta.</p>
+                  <p>Si el país asociado a tu cuenta no coincide con tu país de residencia, puede deberse a que vives en un país y trabajas en otro, a que has instalado una red privada virtual (VPN) para enmascarar tu dirección IP, o a que vives cerca de una frontera entre territorios. Si crees que el país asociado a tu cuenta no es correcto, <span>ponte en contacto con nosotros<i class="fas fa-external-link-alt"></i></span></p>'                                                                                                  ]
+
+  ],
+
+  '¿Cómo puedo eliminar información sobre mi mismo de los resultados de búsqueda de Google?' => [
+       [
+         'Los resultados de búsqueda de Google reflejan el contenido disponible públicamente para todos los usuarios en Internet. Los motores de búsqueda no pueden eliminar contenido directamente de los sitios web. Por ello, eliminar resultados de búsqueda de Google no implica eliminar el contenido de Internet. Si quieres eliminar contenido de Internet, debes <span>ponerte en contacto con el webmaster</span> del sitio web en el que aparece el contenido y pedirle que lo modifique. Asimismo, si en virtud de la normativa europea sobre protección de datos quieres solicitar la eliminación de determinada información sobre ti que aparezca en los resultados de búsqueda de Google, <span>haz clic aquí</span>. Cuando el contenido se haya eliminado y Google detecte la actualización, la información dejará de aparecer en los resultados de búsqueda de Google. Para solicitar una eliminación urgente de contenido también puedes <span>acceder a la página de ayuda para obtener más información.</span>'
+       ]
+   ],
+   '¿¿Mis consultas de búsqueda se envían a los sitios web al hacer clic en los resultados de la Búsqueda de Google?' => [
+        [
+          'En algunos casos, sí. Al hacer clic en un resultado de la Búsqueda de Google, tu navegador web también puede enviar la dirección de Internet (o URL) de la página de resultados de búsqueda a la página web de destino como <span>URL de referencia.</span> La URL de la página de los resultados de búsqueda puede incluir en algunas ocasiones la consulta de búsqueda realizada. Si utilizas la función de búsqueda cifrada de Google, denominada búsqueda SSL, en la mayoría de los casos los términos de búsqueda no aparecerán en la URL de referencia. Existen varias excepciones a esto, por ejemplo, si utilizas determinados navegadores menos populares. Consulta más información sobre la <span>búsqueda SSL.</span> Las consultas de búsqueda o la información incluida en las URL de referencia pueden estar disponibles a través de Google Analytics o de una interfaz de programación de aplicaciones (API). Además, los anunciantes pueden recibir información relacionada con las palabras clave exactas que han dado lugar a un clic en un anuncio.'
+        ]
+    ],
+  ]
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="./dist/css/master.css">
+  </head>
+  <body>
+    <header>
+      <div class="container title">
+        <div class="row">
+          <div class="col-12">
+            <img src="./img/logo_google.png" alt="">
+            <h3>Privacidad y Condiciones</h3>
+          </div>
+        </div>
+      </div>
+      <div class="container menu">
+        <div class="row">
+          <div class="col-12">
+            <ul>
+              <li><a href="#">Descripción general</a></li>
+              <li><a href="#">Política de Privacidad</a></li>
+              <li><a href="#">Condiciones de Servicio</a></li>
+              <li><a href="#">Tecnologías</a></li>
+              <li><a href="#">Preguntas frecuentes</a></li>
+            </ul>
+          </div>
+
+        </div>
+      </div>
+    </header>
+    <main>
+      <div class="container faq">
+        <div class="row">
+          <div class="col-12">
+            <?php foreach ($faqs as $keys => $faq) {
+              echo '<div class="sezione">';
+              echo '<h2>'. $keys .'</h2>';
+              if ($keys === '¿Por qué está asociada mi cuenta a un país?') {
+                foreach ($faq as $infos => $informations) {
+                  foreach ($informations as $others => $others3) {
+                    echo $others3 . '<br>';
+                  }
+                }
+              }
+              else {
+                foreach ($faq as $n) {
+                  foreach ($n as $info) {
+                    echo '<p>'.$info.'</p>';
+                  }
+                }
+              }
+              echo '</div>';
+            }
+             ?>
+          </div>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <div class="container footer_bar">
+        <div class="row">
+          <div class="col-12">
+            <div class="footer_menu">
+              <ul>
+                <li>Google</li>
+                <li>Acerca de Google</li>
+                <li>Privacidad</li>
+                <li>Términos</li>
+              </ul>
+            </div>
+            <div class="lenguas">
+              <i class="fas fa-globe"></i>
+              <select class="form-select" aria-label="Default select example">
+                <option selected>Español</option>
+                <option value="1">Italiano</option>
+                <option value="2">English</option>
+                <option value="3">Chino</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+  </body>
+</html>
